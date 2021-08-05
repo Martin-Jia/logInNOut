8fb1366a86a069e9b9b7263b0acd499793452fc6
Mingda Jia <mingj@microsoft.com>
2021-08-05 17:21:30 +0800

    logic refine

--- src/userLoginTool/app.py
+++ src/userLoginTool/app.py
@@ -7,7 +7,6 @@ import random
 import string
 import time
 import hashlib
-from threading import Thread, Condition
 import jwt
 
 
@@ -18,43 +17,13 @@ class LoginNOut:
         self._db_connector = None
         self._logger = logging.getLogger("LoginNOut")
         self._token_salt = ''
-        self._body_salt = ''
-        self._body_salt_expire = 0.0
-        self._thread = Thread(target=self._update_body_salt, args=(), daemon=True)
-        self._thread_should_stop = Condition()
+        if not self._db_connection_string or not self._db_name:
+            raise self_defined_errors.InitError("must specify the database connection string and a database name")
+        self._db_connector = DatabaseConnector(self._db_connection_string, self._db_name)        
 
     def _gen_salt(self, salt_len: int) -> str:
         return ''.join(random.sample(string.ascii_letters + string.digits, salt_len))  
 
-    def _update_body_salt(self):
-        self._thread_should_stop.acquire()
-        while True:
-            salt_len = random.randint(Constants.SALT_LEN_MIN, Constants.SALT_LEN_MAX)
-            self._body_salt = self._gen_salt(salt_len)
-            self._body_salt_expire = datetime.datetime.utcnow().timestamp() + Constants.SALT_EXPIRE_TIME
-            value = self._thread_should_stop.wait(Constants.SALT_EXPIRE_TIME)
-            if value:
-                break
-
-    def start(self):
-        if self._db_connector is None:
-            if not self._db_connection_string or not self._db_name:
-                raise self_defined_errors.InitError("must specify the database connection string and a database name")
-            self._db_connector = DatabaseConnector(self._db_connection_string, self._db_name)
-        if not self._thread.is_alive():
-            salt_len = random.randint(Constants.SALT_LEN_MIN, Constants.SALT_LEN_MAX)
-            self._token_salt = self._gen_salt(salt_len)
-            self._thread.start()
-            while self._body_salt == '':
-                time.sleep(1)
-
-    def stop(self):
-        self._thread_should_stop.acquire()
-        self._thread_should_stop.notify()
-        self._thread_should_stop.release()
-
-    def get_current_salt(self):
-        return self._body_salt, self._body_salt_expire - datetime.datetime.utcnow().timestamp()
 
     def varify_token(self, token: str):
         try:
@@ -84,16 +53,7 @@ class LoginNOut:
             return None   
 
 
-    def get_token(self, body):
-        try:
-            decoded_body = jwt.decode(body, self._body_salt, algorithms=["HS256"])
-        except jwt.exceptions.InvalidTokenError:   
-            self._logger.warn('invalid token')         
-            return None   
-        if 'username' not in decoded_body or 'password' not in decoded_body:
-            return None
-        username = decoded_body['username']
-        password = decoded_body['password']
+    def get_token(self, username, password):
         user = self._db_connector.query_user_with_username(username)
         if not user:
             return None
@@ -115,16 +75,7 @@ class LoginNOut:
             }
             return jwt.encode(ret, self._token_salt, algorithm="HS256")
 
-    def register_user(self, body):
-        try:
-            decoded_body = jwt.decode(body, self._body_salt, algorithms=["HS256"])
-        except jwt.exceptions.InvalidTokenError:   
-            self._logger.warn('invalid token')         
-            return None   
-        if 'username' not in decoded_body or 'password' not in decoded_body:
-            return None
-        username = decoded_body['username']
-        password = decoded_body['password']        
+    def register_user(self, username, password):    
         user = self._db_connector.query_user_with_username(username)
         if user:
             raise self_defined_errors.UserError('user already exists')
--- tests/test_user_login_tool.py
+++ tests/test_user_login_tool.py
@@ -12,19 +12,8 @@ class TestUserLoginTool(unittest.TestCase):
     @mock.patch('userLoginTool.utils.db_helper.DatabaseConnector.query_user_with_username', return_value=None)
     def test_register(self, mock_mongo, find_one):
         loginNOut = userLoginTool.LoginNOut("fake_db_name", "fake_db_connection_str")
-        loginNOut.start()
-        s, expire = loginNOut.get_current_salt()
-        body = jwt.encode({'username': 'foo', 'password': 'bar'}, s, algorithm='HS256')
-        loginNOut.register_user(body)
-        loginNOut.stop()
+        loginNOut.register_user('foo', 'bar')
 
-
-    def test_get_salt(self):
-        loginNOut = userLoginTool.LoginNOut("fake_db_name", "fake_db_connection_str")
-        loginNOut.start()
-        s, expire = loginNOut.get_current_salt()
-        loginNOut.stop()
-        assert s
 
     @mock.patch('pymongo.MongoClient')
     @mock.patch('userLoginTool.utils.db_helper.DatabaseConnector.query_user_with_username', return_value={
@@ -32,30 +21,22 @@ class TestUserLoginTool(unittest.TestCase):
         'salt': 'LptMXaj'
     })
     def test_login(self, client, query):
-        loginNOut = userLoginTool.LoginNOut("fake_db_name", "fake_db_connection_str")
-        loginNOut.start()        
-        s, expire = loginNOut.get_current_salt()
-        body = jwt.encode({'username': 'foo', 'password': 'bar'}, s, algorithm='HS256')        
-        token = loginNOut.get_token(body)
-        loginNOut.stop()
+        loginNOut = userLoginTool.LoginNOut("fake_db_name", "fake_db_connection_str")       
+        token = loginNOut.get_token('foo', 'bar')
         assert token
 
     @mock.patch('pymongo.MongoClient')
     def test_verify_token(self, client):
-        loginNOut = userLoginTool.LoginNOut("fake_db_name", "fake_db_connection_str")
-        loginNOut.start()        
-        s, expire = loginNOut.get_current_salt()
-        body = jwt.encode({'username': 'foo', 'password': 'bar'}, s, algorithm='HS256')    
+        loginNOut = userLoginTool.LoginNOut("fake_db_name", "fake_db_connection_str")  
         with mock.patch('userLoginTool.utils.db_helper.DatabaseConnector.query_user_with_username', return_value={
             'password': b'\xd9s\xac\x7f\x17\xd7\x9fH)8l3\xf4\x88\xd66!\xe7\xb9\xfd\x919\x9e\xae\xdc\xa5\xb1|u5\xe2\xb9',
             'salt': 'LptMXaj'
         }):
-            token = loginNOut.get_token(body)
+            token = loginNOut.get_token('foo', 'bar')
         token_body = jwt.decode(token, loginNOut._token_salt, algorithms=['HS256'])
         user_token = token_body['data']['token']
         with mock.patch('userLoginTool.utils.db_helper.DatabaseConnector.query_user_token', return_value={'token': user_token, 'expire_time': float('inf')}):
             user = loginNOut.varify_token(token)
-            loginNOut.stop()
             assert user
     
 
